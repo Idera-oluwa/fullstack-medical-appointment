93e6c2fb22ffe954781dfa4943226b06273602c0
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-09-28 08:11:01 -0700

    patient's profile account created

--- frontend/src/app/profile/myaccount/page.js
+++ frontend/src/app/profile/myaccount/page.js
@@ -8,6 +8,7 @@ import {TbHome} from 'react-icons/tb'
 import {BsChatLeftDots,BsPerson} from 'react-icons/bs'
 import {IoMdSearch} from 'react-icons/io'
 import Link from 'next/link'
+import Footer from '../../Components/footer'
 
 const page = () => {
   return (
@@ -79,30 +80,7 @@ const page = () => {
     <div className='outline outline-1 outline-gray-200 w-[2rem] h-[2rem] rounded-full flex items-center justify-center'><PiCaretRightBold className='text-dark cursor-pointer'/></div>
     </div>
     </div>
-    <div className='p-[1rem] fixed bottom-[1rem] w-full right-0'>
-    <div className='flex flex-row items-center justify-between bg-white px-[2rem] py-[1rem] mt-[2rem] outline outline-1 outline-gray-200 rounded-[10px]'>
-    <div className='flex flex-col text-secondary gap-[0.3rem] cursor-pointer items-center'>
-      <TbHome className='text-[1.4rem]'/>
-      <p className='text-[0.6rem]'>HOME</p>
-    </div>
-    <div className='flex flex-col text-secondary gap-[0.3rem] cursor-pointer items-center'>
-      <IoMdSearch className='text-[1.4rem]'/>
-      <p className='text-[0.6rem]'>SEARCH</p>
-    </div>
-    <div className='flex flex-col text-secondary gap-[0.3rem] cursor-pointer items-center'>
-      <BiVideo className='text-[1.4rem]'/>
-      <p className='text-[0.6rem]'>VIDEO</p>
-    </div>
-    <div className='flex flex-col text-secondary gap-[0.3rem] cursor-pointer items-center'>
-      <BsChatLeftDots className='text-[1.4rem]'/>
-      <p className='text-[0.6rem]'>CHAT</p>
-    </div> 
-    <div className='flex flex-col text-secondary gap-[0.3rem] cursor-pointer items-center'>
-      <BsPerson className='text-[1.4rem]'/>
-      <p className='text-[0.6rem]'>PROFILE</p>
-    </div>
-    </div>
-    </div>
+    <Footer/>
     </div>
     </div>
   )
