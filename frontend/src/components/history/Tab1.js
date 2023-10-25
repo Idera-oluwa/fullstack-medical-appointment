56292c11cdbf120f4af4db0e822f15005de131ba
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-10-25 13:14:57 -0700

    history page updated

--- frontend/src/components/history/Tab1.js
+++ frontend/src/components/history/Tab1.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Tab1 = () => {
   return (
-    <div>
+    <div className='pb-[8rem]'>
       <p className='font-semibold text-[17px] px-[1rem]'>Today</p>
       <div className='flex flex-col gap-[0.3rem] mt-[0.5rem]'>
         <div className='flex flex-row gap-[1rem] bg-white p-[1rem]'>
@@ -10,9 +10,63 @@ const Tab1 = () => {
            alt=''
            className='w-[40px] h-[40px] rounded-[10px]'
            />
-           <div className='flex flex-col gap-[0.5rem] '>
-             <p className='font-semibold text-dark text-[16px]'>Dr. Cayden Stack</p>
-             <p className='text-secondary text-[12px]'>I am cardio patient. I need your help</p>
+           <div className='flex flex-col gap-[0.2rem] '>
+             <p className='font-medium text-dark text-[15px] mb-0'>Dr. Cayden Stack</p>
+             <p className='text-secondary text-[11px] mt-0'>I am cardio patient. I need your help</p>
+           </div>
+        </div>
+        <div className='flex flex-row gap-[1rem] bg-white p-[1rem]'>
+           <img src='/Images/top-doctor-2.jpg'
+           alt=''
+           className='w-[40px] h-[40px] rounded-[10px]'
+           />
+           <div className='flex flex-col gap-[0.2rem] '>
+             <p className='font-medium text-dark text-[15px] mb-0'>Dr. Cayden Stack</p>
+             <p className='text-secondary text-[11px] mt-0'>I am cardio patient. I need your help</p>
+           </div>
+        </div>
+        <div className='flex flex-row gap-[1rem] bg-white p-[1rem]'>
+           <img src='/Images/top-doctor-2.jpg'
+           alt=''
+           className='w-[40px] h-[40px] rounded-[10px]'
+           />
+           <div className='flex flex-col gap-[0.2rem] '>
+             <p className='font-medium text-dark text-[15px] mb-0'>Dr. Cayden Stack</p>
+             <p className='text-secondary text-[11px] mt-0'>I am cardio patient. I need your help</p>
+           </div>
+        </div>
+      </div>
+
+      <p className='font-semibold text-[17px] px-[1rem] mt-[3rem]'>Yesterday</p>
+      <div className='flex flex-col gap-[0.3rem] mt-[0.5rem]'>
+        <div className='flex flex-row gap-[1rem] bg-white p-[1rem]'>
+           <img src='/Images/top-doctor-2.jpg'
+           alt=''
+           className='w-[40px] h-[40px] rounded-[10px]'
+           />
+           <div className='flex flex-col gap-[0.2rem] '>
+             <p className='font-medium text-dark text-[15px] mb-0'>Dr. Cayden Stack</p>
+             <p className='text-secondary text-[11px] mt-0'>I am cardio patient. I need your help</p>
+           </div>
+        </div>
+        <div className='flex flex-row gap-[1rem] bg-white p-[1rem]'>
+           <img src='/Images/top-doctor-2.jpg'
+           alt=''
+           className='w-[40px] h-[40px] rounded-[10px]'
+           />
+           <div className='flex flex-col gap-[0.2rem] '>
+             <p className='font-medium text-dark text-[15px] mb-0'>Dr. Cayden Stack</p>
+             <p className='text-secondary text-[11px] mt-0'>I am cardio patient. I need your help</p>
+           </div>
+        </div>
+        <div className='flex flex-row gap-[1rem] bg-white p-[1rem]'>
+           <img src='/Images/top-doctor-2.jpg'
+           alt=''
+           className='w-[40px] h-[40px] rounded-[10px]'
+           />
+           <div className='flex flex-col gap-[0.2rem] '>
+             <p className='font-medium text-dark text-[15px] mb-0'>Dr. Cayden Stack</p>
+             <p className='text-secondary text-[11px] mt-0'>I am cardio patient. I need your help</p>
            </div>
         </div>
       </div>
