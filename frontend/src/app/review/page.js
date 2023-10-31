ff608e18d2a03e57d9da249b2eba8aaf88e06c90
Idera_oluwa <ideraoluwaisa2003@gmail.com>
2023-10-31 12:06:17 -0700

    new commit

---
+++ frontend/src/app/review/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "../../components/profilePage/Header";
+
+const page = () => {
+  return (
+    <div className='bg-light'>
+      <Header text='My Appointment'/>
+      <div className='mt-4'>
+      <p>I'm paragraph and I'm good at being a paragraph. I'd work on this tomorrow</p>
+      </div>
+    </div>
+  )
+}
+
+export default page
